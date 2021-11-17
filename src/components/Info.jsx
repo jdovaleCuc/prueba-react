8f01de71b827968ebc2b76e9a3bb44ff9dde8577
jdovaleCuc <joeldovalear12@gmail.com>
2021-11-17 13:26:23 -0500

    CommitCountry

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,30 +1,34 @@
 import React from 'react';
-import { Table } from 'react-bootstrap';
+import { Container, Table } from 'react-bootstrap';
 
 const Info = ({ data }) => {
     return (
-        <Table striped bordered hover variant='dark'>
-            <thead>
-                <tr>
-                    <th>Casos Confirmados</th>
-                    <th>Continente</th>
-                    <th>Poblacion</th>
-                    <th>Area del Pais</th>
-                    <th>Numero de muertes</th>
-                    <th>Expectativa de vida</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>{data.confirmed}</td>
-                    <td>{data.continent}</td>
-                    <td>{data.population}</td>
-                    <td>{data.location}</td>
-                    <td>{data.deaths}</td>
-                    <td>{data.life_expectancy}</td>
-                </tr>
-            </tbody>
-        </Table>
+        <Container>
+            <Table striped bordered hover variant='dark'>
+                <thead>
+                    <tr>
+                        <th>Pais</th>
+                        <th>Casos Confirmados</th>
+                        <th>Continente</th>
+                        <th>Poblacion</th>
+                        <th>Area del Pais</th>
+                        <th>Numero de muertes</th>
+                        <th>Expectativa de vida</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{data.country}</td>
+                        <td>{data.confirmed}</td>
+                        <td>{data.continent}</td>
+                        <td>{data.population}</td>
+                        <td>{data.location}</td>
+                        <td>{data.deaths}</td>
+                        <td>{data.life_expectancy}</td>
+                    </tr>
+                </tbody>
+            </Table>
+        </Container>
     );
 };
 
